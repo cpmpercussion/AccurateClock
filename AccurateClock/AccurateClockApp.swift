00cc7b75a95563cfd50266795c3a478a005355e1
Charles Martin <charles.martin@anu.edu.au>
2026-05-09 08:21:50 +1000

    Initial Commit

---
+++ AccurateClock/AccurateClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccurateClockApp.swift
+//  AccurateClock
+//
+//  Created by Charles Martin on 9/5/2026.
+//
+
+import SwiftUI
+
+@main
+struct AccurateClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AccurateClockTests/AccurateClockTests.swift
@@ -0,0 +1,18 @@
+//
+//  AccurateClockTests.swift
+//  AccurateClockTests
+//
+//  Created by Charles Martin on 9/5/2026.
+//
+
+import Testing
+
+struct AccurateClockTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
